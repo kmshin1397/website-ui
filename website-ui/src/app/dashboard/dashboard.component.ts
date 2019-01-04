365b5035cd09cfb988298a0aba642de7f2ab46c4
Kyung Min Shin <kshin@caltech.edu>
2019-01-04 18:27:03 -0500

    Finish implementing landing page

--- website-ui/src/app/dashboard/dashboard.component.ts
+++ website-ui/src/app/dashboard/dashboard.component.ts
@@ -8,11 +8,13 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class DashboardComponent implements OnInit {
     @ViewChild('espresso') espresso: ElementRef;
     @ViewChild('coffee') coffee: ElementRef;
+    @ViewChild('espressoText') espressoText: ElementRef;
+    @ViewChild('coffeeText') coffeeText: ElementRef;
+    @ViewChild('introText') introText: ElementRef;
     @ViewChild('stripL') stripL: ElementRef;
     @ViewChild('stripR') stripR: ElementRef;
     @ViewChild('stripM') stripM: ElementRef;
-
-    private baseOpacity: number = 0.6;
+    @ViewChild('stripM2') stripM2: ElementRef;
 
     constructor() {}
 
@@ -20,22 +22,30 @@ export class DashboardComponent implements OnInit {
 
     hoverLogo(mouseEnter: boolean, type: string) {
         if (mouseEnter) {
-            this.stripL.nativeElement.style.opacity = 1;
-            this.stripM.nativeElement.style.opacity = 1;
-            this.stripR.nativeElement.style.opacity = 1;
+            this.stripL.nativeElement.classList.toggle('active');
+            this.stripM.nativeElement.classList.toggle('active');
+            this.stripM2.nativeElement.classList.toggle('active');
+            this.stripR.nativeElement.classList.toggle('active');
+            this.introText.nativeElement.classList.toggle('active');
             if (type === 'espresso') {
-                this.espresso.nativeElement.style.opacity = 1;
+                this.espresso.nativeElement.classList.toggle('active');
+                this.espressoText.nativeElement.classList.toggle('active');
             } else {
-                this.coffee.nativeElement.style.opacity = 1;
+                this.coffee.nativeElement.classList.toggle('active');
+                this.coffeeText.nativeElement.classList.toggle('active');
             }
         } else {
-            this.stripL.nativeElement.style.opacity = this.baseOpacity;
-            this.stripM.nativeElement.style.opacity = this.baseOpacity;
-            this.stripR.nativeElement.style.opacity = this.baseOpacity;
+            this.stripL.nativeElement.classList.toggle('active');
+            this.stripM.nativeElement.classList.toggle('active');
+            this.stripM2.nativeElement.classList.toggle('active');
+            this.stripR.nativeElement.classList.toggle('active');
+            this.introText.nativeElement.classList.toggle('active');
             if (type === 'espresso') {
-                this.espresso.nativeElement.style.opacity = this.baseOpacity;
+                this.espresso.nativeElement.classList.toggle('active');
+                this.espressoText.nativeElement.classList.toggle('active');
             } else {
-                this.coffee.nativeElement.style.opacity = this.baseOpacity;
+                this.coffee.nativeElement.classList.toggle('active');
+                this.coffeeText.nativeElement.classList.toggle('active');
             }
         }
     }
--- website-ui/src/app/logo/logo.component.ts
+++ website-ui/src/app/logo/logo.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class LogoComponent implements OnInit {
     private espresso: boolean = false;
     private coffee: boolean = false;
+    private profile: boolean = false;
 
     @Input()
     set type(type: string) {
@@ -16,6 +17,8 @@ export class LogoComponent implements OnInit {
             this.espresso = true;
         } else if (type === 'coffee') {
             this.coffee = true;
+        } else if (type === 'profile') {
+            this.profile = true;
         }
     }
 
